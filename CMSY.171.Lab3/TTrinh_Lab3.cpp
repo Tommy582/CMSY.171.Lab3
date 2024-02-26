68a9366984866a7c8e6d44638d07a31ca855a5b9
Tommy <tommytrinh582@yahoo.com>
2024-02-25 20:12:28 -0500

    Add project files.

---
+++ CMSY.171.Lab3/TTrinh_Lab3.cpp
@@ -0,0 +1,473 @@
+// MAKE SURE FILEPATH IS CORRECT AND IS A CONSTANT
+// 
+// This program simulates a database that can read and write to
+// text and binary files
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <cctype>
+#include <fstream>
+using namespace std;
+
+// declare constants
+const int
+MIN_COUNT = 0,			// minimum number of animals
+ADD_CHOICE = 1,			// choice to add an animal
+DISPLAY_CHOICE = 2,		// choice to display animals
+SEARCH_CHOICE = 3,
+ENDANGERED_CHOICE = 4,	// choice to display endangered animals
+QUIT_CHOICE = 5,		// choice to quit program
+MENU_CHOICE_SIZE = 1,	// size of menu choice field
+ENDANGERED_POP = 100,	// population considered endangered
+TYPE_SIZE = 25;			// size of the animal type field
+
+// define structures
+struct species
+{
+	char typeAnimal[TYPE_SIZE] = "none";	// stores type of animal
+	char species[TYPE_SIZE];				// species of animal
+	int numAnimal = 0;						// stores number of animals
+	bool endangered = false;				// flag for endangered
+};
+
+// define function prototypes
+void header();							// display program info to user
+void dataEntry(vector<species>&,		// allows data entry of animals
+	const vector<string>,fstream&);
+void dataDisplay(vector<species>);		// displays data to user
+void endangeredDisplay(const vector<species>);// displays endangered animals
+void selectionSort(vector<species>&);	// sorts an array by selection sorting
+void vectorSwap(species&, species&);	// swaps two vector elements
+void animalSearch(vector<species>&);	// sorts and then searches for an animal
+void readAnimal(vector<species>&, fstream&);// reads data from a file into memory
+void readSpecies(vector<string>&, fstream&);// reads data from a file into memory
+bool positiveValid(int);				// validates a number is positive
+bool menuValid(int);					// validates menu is between first and quit
+bool noneValid(const char name[]);		// validates user input is not none
+bool duplicateValid(const vector<species>,
+	const char name[]);					// validates user has not entered duplicate
+bool exitProgram();						// ask user if they really want to quit
+
+int main()
+{
+	// define variables
+	string input;					// temporary string for user input
+	int menuChoice;					// user entered menu choice
+	vector<species> animalPen;		// initialize a new vector for struct
+	vector<string> speciesType;		// initialize a new vector for species
+
+	// Open the animal.dat file in read/write/binary mode
+	fstream animalFile("animal.dat", ios::in | ios::out | ios::binary);
+
+	// If the file was opened, read the contents of animal.dat into memory.
+	// Otherwise, give an error and end the program
+	if (animalFile.fail())
+	{
+		cout << "Error - could not open animal.dat. Closing the program.";
+		return 0;
+	}
+	else
+		readAnimal(animalPen, animalFile);
+
+	// open the species.txt in read mode
+	fstream speciesFile("species.txt", ios::in);
+	
+	// If the file was opened, read the contents of species.txt into memory.
+	// Otherwise, give an error and end the program
+	if (!speciesFile)
+	{
+		cout << "Error - could not open species.txt. Closing the program.";
+		return 0;
+	}
+	else
+		readSpecies(speciesType, speciesFile);
+
+	// call header
+	header();
+
+	do
+	{
+		// display a menu and  ask user for a menu choice
+		do
+		{
+			cout << ADD_CHOICE << ". Add animal(s)\n";
+			cout << DISPLAY_CHOICE << ". Display animals\n";
+			cout << SEARCH_CHOICE << ". Search for animal\n";
+			cout << ENDANGERED_CHOICE << ". Display endangered animals\n";
+			cout << QUIT_CHOICE << ". Quit\n\n";
+			cout << "Enter the menu choice: ";
+			getline(cin, input);
+		} while (menuValid(stoi(input)));
+		menuChoice = stoi(input);
+
+		// switch statement based on user choice
+		switch (menuChoice)
+		{
+		case ADD_CHOICE:
+		{
+			dataEntry(animalPen, speciesType, animalFile);
+			break;
+		}
+		case DISPLAY_CHOICE:
+		{
+			dataDisplay(animalPen);
+			break;
+		}
+		case SEARCH_CHOICE:
+		{
+			animalSearch(animalPen);
+			break;
+		}
+		case ENDANGERED_CHOICE:
+		{
+			endangeredDisplay(animalPen);
+			break;
+		}
+		case QUIT_CHOICE:
+		{
+			if (exitProgram())
+				menuChoice = ADD_CHOICE;
+			break;
+		}
+		default:
+		{
+			cout << "Something went wrong\n\n";
+			break;
+		}
+		}
+	} while (menuChoice != QUIT_CHOICE);
+
+	// clear the vector
+	animalPen.clear();
+	// close the files
+	animalFile.close();
+	speciesFile.close();
+
+	// goodbye message
+	cout << "\nThank you for using the CMSY 171 Animal Count Program\n\n";
+
+	// end of main
+	system("pause");
+	return 0;
+}
+
+// shows program title, copyright, and program info to user
+void header()
+{
+	cout << "\t\t\t\tCMSY 171 Lab 3\n"
+		<< "Copyright 2024 - Howard Community College All rights reserved;"
+		<< "Unauthorized duplication prohibited.\n\n"
+		<< "\t\t\tWelcome to CMSY-171 Animal Count Program\n\n";
+}
+
+
+// validates menu choice, returns true if invalid and keeps looping, returns false if
+// valid and exits loop
+bool menuValid(int choice)
+{
+	if (ADD_CHOICE > choice || choice > QUIT_CHOICE)
+	{
+		cout << "Invalid choice! Try again: \n\n";
+		return true;
+	}
+	return false;
+}
+
+// tests if animal count is positive, returns true if invalid and keeps looping,
+// returns false if valid and exits loop
+bool positiveValid(int num)
+{
+	if (num < MIN_COUNT)
+	{
+		cout << "Invalid count! Try again\n";
+		return true;
+	}
+	return false;
+}
+
+// tests if user entered none. If user entered none, return true and stop loop
+// if not none, return false and ask for more info
+bool noneValid(const char name[])
+{
+	char temp[TYPE_SIZE];	// temp cstring to test none
+	for (int index = 0; index < TYPE_SIZE; index++)
+		temp[index] = toupper(name[index]);
+	if (!strcmp(temp, "NONE"))
+		return true;
+	return false;
+}
+
+// ask user if they really want to quit, return true if they do not and keep
+// looping, return false if they want to quit and leave loop
+bool exitProgram()
+{
+	char temp[TYPE_SIZE];	// holds user's choice to quit
+	cout << "\nDo you wish to exit the program (Y or N): ";
+	cin.getline(temp, TYPE_SIZE);
+	while (toupper(temp[0]) != 'Y' && toupper(temp[0]) != 'N')
+	{
+		cout << "Please enter Y or N: ";
+		cin.getline(temp, TYPE_SIZE);
+	}
+	if (toupper(temp[0]) == 'Y')
+		return false;
+	else
+		return true;
+}
+
+// Reads structure data from a file and copies it into memory
+void readAnimal(vector<species>& animalPen, fstream &animalFile)
+{
+	species tempAnimal={"","",0,false},	// temporary structure for push back
+		tempFile;		// temporary structure for file
+
+	// read the first record
+	animalFile.read(reinterpret_cast<char*>(&tempFile), sizeof(tempFile));
+	while (!animalFile.eof())
+	{
+		// c strings are arrays so go through them one index at a time
+		for (int index = 0; index < strlen(tempFile.typeAnimal); index++)
+			tempAnimal.typeAnimal[index] = tempFile.typeAnimal[index];
+		// same for species, one index at a time
+		for (int index = 0; index < strlen(tempFile.species); index++)
+			tempAnimal.species[index] = tempFile.species[index];
+		// copy over the count and endangered flag
+		tempAnimal.numAnimal = tempFile.numAnimal;
+		tempAnimal.endangered = tempFile.endangered;
+		// push back the vector with a new entry
+		animalPen.push_back(tempAnimal);
+		// clear the temp structure in preparation for the next loop
+		tempAnimal = { "","",0,false };
+		// read the next record
+		animalFile.read(reinterpret_cast<char*>(&tempFile), sizeof(tempFile));
+	}
+}
+
+// Read species data from a file and copy it into memory
+void readSpecies(vector<string>& speciesType, fstream& speciesFile)
+{
+	string temp;	// temp string
+	// read the first record
+	getline(speciesFile, temp, '\n');
+	// if successful, popback into vector and continue
+	while (speciesFile)
+	{
+		speciesType.push_back(temp);
+		getline(speciesFile,temp,'\n');
+	}
+}
+
+// lets user enter data on the animal types, if user enters none, quit out
+// Added "&" to the argument because otherwise the changes were local
+// and did not appear to the dataDisplay function
+void dataEntry(vector<species>& animalPen, const vector<string>speciesType,
+	fstream& animalFile)
+{
+	string input;				// temporary input string
+	bool noneFlag = true;		// flag for none 
+	species newAnimal = { "","",0,false };	// temporary struct for data entry
+
+	// loop until none input
+	while (noneFlag)
+	{
+		do // ask user for animal name, if duplicate then repeat prompt
+		{  // if not duplicate, or user entered none, stop looping
+			cout << "Please enter an animal type (none to stop): ";
+			cin.getline(newAnimal.typeAnimal, TYPE_SIZE);
+		} while (duplicateValid(animalPen, newAnimal.typeAnimal));
+		// if user types none, set flag to false and exit loop
+		if (noneValid(newAnimal.typeAnimal))
+			noneFlag = false;
+
+		// if not none and not duplicate, ask for the rest of info. 
+		// Added endangered=false because it would always be true once 
+		// one entry is true, never false anymore
+		if (noneFlag)
+		{
+			do
+			{
+				// ask for the animal species
+				cout << "\nSelect the species from the list:\n";
+				for (int index = 0; index < speciesType.size(); index++)
+					cout << index+1<<". "<<speciesType[index] << endl;
+				cout << "Please enter number of species here: ";
+				getline(cin, input);
+				while (stoi(input) < 1 || stoi(input) > speciesType.size())
+				{ // validate it is between 1 and max
+					cout << "Error - entry must be a valid option. Try again: ";
+					getline(cin, input);
+				}
+				// copy the species into the struct
+				for (int index = 0; index < speciesType[stoi(input)-1].length(); index++)
+				{
+					if (isalpha(speciesType[stoi(input) - 1][index]))
+					{
+						newAnimal.species[index] = speciesType[stoi(input) - 1][index];
+					}
+					else
+						newAnimal.species[index] = '\0';
+				}
+
+				cout << "Enter the animal type's count: ";
+				getline(cin, input);
+			} while (positiveValid(stoi(input)));
+			newAnimal.numAnimal = stoi(input);
+			if (newAnimal.numAnimal < ENDANGERED_POP)
+				newAnimal.endangered = true;
+			else
+				newAnimal.endangered = false;
+			// write data from temp struct to animal.dat file
+			animalFile.write(reinterpret_cast<char*>(&newAnimal), sizeof(newAnimal));
+			// Add data from temp struct to permanent struct
+			animalPen.push_back(newAnimal);
+		}
+	}
+	cout << endl;
+}
+
+// test if name is a duplicate. If it is, return true and keep looping
+// if not, return false and exit loop
+bool duplicateValid(const vector <species> animal, const char name[])
+{
+	for (auto index : animal)
+		if (!strcmp(index.typeAnimal, name))
+		{
+			cout << "Error - this animal is already in the database. "
+				<< "Please reenter.\n";
+			return true;
+		}
+	return false;
+}
+
+
+// display contents of animal array to user, if an entry has 0 number of
+// animals, end the loop and redisplay menu
+void dataDisplay(vector<species> animals)
+{
+	// if the vector is empty, give an error 
+	if (animals.empty())
+		cout << "You have not entered any data, returning to menu.\n\n";
+	else
+	{
+		// send the vector to a sort function
+		selectionSort(animals);
+		cout << "\nThere are " << animals.size() << " animals in the database.\n";
+		for (int index = 0; index < animals.size(); index++)
+		{
+			cout << "\nAnimal: " << animals.at(index).typeAnimal << endl;
+			cout << "The species is: " << animals.at(index).species << endl;
+			cout << "Has a count of: " << animals.at(index).numAnimal << endl;
+
+			if (animals.at(index).endangered)
+				cout << "This animal is endangered!!\n\n";
+			else
+				cout << "This animal is not endangered!!\n\n";
+		}
+
+	}
+}
+
+// display endangered species to user, if there are no endangered species
+// then counter variable is equal to 0, which is the same as false
+// if counter is incremented, it is not 0, which is the same as true
+// if counter is false, display error message and redisplay menu
+void endangeredDisplay(const vector<species> animals)
+{
+	int counter = 0;	// temporary counter variable
+	for (auto status : animals)
+		if (status.endangered)
+		{
+			cout << "Animal: " << status.typeAnimal << " is endangered!!\n";
+			counter++;
+		}
+	if (!counter)
+		cout << "There are no endangered species. Redisplaying menu.\n";
+	cout << endl;
+}
+
+// Sort an array using selection sorting in alphabetical order according
+// to the ADT animal type/name
+void selectionSort(vector<species>& animal)
+{
+	string min;
+	int index;
+
+	for (int i = 0; i < animal.size(); i++)
+	{
+		index = i;
+		min = animal.at(i).typeAnimal;
+		for (int j = i + 1; j < animal.size(); j++)
+		{
+			if (animal.at(j).typeAnimal < min)
+			{
+				index = j;
+				min = animal.at(j).typeAnimal;
+			}
+		}
+		vectorSwap(animal.at(index), animal.at(i));
+	}
+}
+
+// Swaps two vector elements, using a temporary ADT
+void vectorSwap(species& a, species& b)
+{
+	species temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+// Sort an ADT and then asks user for a name to search for.
+// Then searches for an animal name using
+// a binary search approach
+void animalSearch(vector<species>& animal)
+{
+	char temp[TYPE_SIZE];		//temporary input holder
+	// first sort by alphabetical order
+	selectionSort(animal);
+	// ask for the name to search for
+	cout << "Please enter the name of the animal you are looking for: ";
+	cin.getline(temp, TYPE_SIZE);
+
+	// set up the search
+	int first = 0,					// first vector element
+		last = animal.size() - 1,	// last vector element
+		middle,						// mid point of search
+		position = -1;				// position of search value
+	bool found = false;				// flag for found
+
+	// search for the entered animal
+	while (!found && first <= last)
+	{
+		middle = (first + last) / 2;// calculate midpoint
+		if (!strcmp(animal.at(middle).typeAnimal, temp))
+		{							// if value is found at mid
+			found = true;
+			position = middle;
+		}
+		else if (strcmp(temp, animal.at(middle).typeAnimal) < 0)
+			last = middle - 1;		// if value is in lower half
+		else
+			first = middle + 1;		// if value is in upper half
+	}
+
+	// display the results of the search
+	if (found)
+	{
+		cout << "Animal was found:\n"
+			<< "Animal: " << animal.at(position).typeAnimal << endl
+			<< "Has a count of: " << animal.at(position).numAnimal << endl;
+		if (animal.at(position).endangered)
+			cout << "This animal is endangered!!\n\n";
+		else
+			cout << "This animal is not endangered!!\n\n";
+	}
+	else
+	{
+		cout << "We could not find your animal in the database at this time. "
+			<< "Please add the animal to the database.\n\n";
+	}
+}
